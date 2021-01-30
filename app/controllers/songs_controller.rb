76ed908ce4c6f93572e0d76860f72cf83b2c2415
Michael Fuget <michaelfuget@gmail.com>
2021-01-30 05:53:43 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,6 @@ class SongsController < ApplicationController
 
   def show
     puts params
-    flash[:alert] = "Song not found."
 
     if params[:artist_id].blank?
       @songs = Song.all
@@ -26,6 +25,7 @@ class SongsController < ApplicationController
       if @song !=nil && @song.artist == @artist
         @songs = @artist.songs
       else
+        binding.pry
         flash[:alert] = "Song not found."
         redirect_to artist_songs_path(@artist)
       end
