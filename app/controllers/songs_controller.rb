2f2c58536c63637ffc3396daf778f7c5f504f385
Michael Fuget <michaelfuget@gmail.com>
2021-01-30 05:52:51 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,7 +26,7 @@ class SongsController < ApplicationController
         @songs = @artist.songs
       else
         flash[:alert] = "Song not found."
-        redirect_to artist_songs_path
+        redirect_to artist_songs_path(@artist)
       end
     end
 
