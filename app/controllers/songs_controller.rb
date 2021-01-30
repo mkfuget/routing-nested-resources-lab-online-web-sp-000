2475f6873b7f19babdc3731cffb892c9cf782e97
Michael Fuget <michaelfuget@gmail.com>
2021-01-30 06:11:08 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,7 +19,7 @@ class SongsController < ApplicationController
     @song = Song.find_by(params[:id])
     if !params[:artist_id].blank?
       @artist = Artist.find_by(id: params[:artist_id])
-      if @artist.songs.find{|song| song == @song} != nil
+      if @artist.songs.find{|song| song == @song}
         puts "found"
         @songs = @artist.songs
       else
