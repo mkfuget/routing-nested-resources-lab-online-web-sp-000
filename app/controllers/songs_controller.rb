2ac2782ffbb467576748c30dc649d6a3681b8ed0
Michael Fuget <michaelfuget@gmail.com>
2021-01-30 06:03:29 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -21,7 +21,7 @@ class SongsController < ApplicationController
     if !params[:artist_id].blank?
 
       @artist = Artist.find_by(id: params[:artist_id])
-      puts @artist.name
+      puts @artist.songs
 
       if @song.artist == @artist
         @songs = @artist.songs
